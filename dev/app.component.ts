b0a1d6d45e5dd13619c90f6e830e56563ededbeb
KZ <zhiping72@gmail.com>
2016-04-02 22:10:05 +0200

    Routing implemented

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,16 +1,24 @@
 import {Component} from 'angular2/core';
+import{RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {Navbar} from './components/Navbar';
 import {Users} from './components/Users';
 import {Posts} from './components/Posts';
 
+
+@RouteConfig([
+    {path: '/', name: 'Users', component: Users, useAsDefault: true},
+    {path: '/posts', name: 'Posts', component: Posts},
+    {path: '/*other', name: 'other', redirectTo: ['Users']}
+])
+
 @Component({
     selector: 'my-app',
-    directives: [Navbar, Users, Posts],
+    directives: [Navbar, Users, Posts, ROUTER_DIRECTIVES],
     template: `
         <nav-bar></nav-bar>
-        <h1>Angular 2 Boilerplate</h1>
-        <users></users>
-        <posts></posts>
+        <h1>Payments</h1>
+        <router-outlet></router-outlet>
     `,
 })
 export class AppComponent {
--- dev/components/Navbar.ts
+++ dev/components/Navbar.ts
@@ -1,13 +1,21 @@
 import {Component} from 'angular2/core';
+import{Router, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'nav-bar',
-    templateUrl: './dev/templates/nav-bar.html'
+    templateUrl: './dev/templates/nav-bar.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class Navbar{
-    constructor(){
+    constructor(private _router: Router){
 
+    }
+
+    isCurrentRoute(routeName){
+        var instruction = this._router.generate(routeName);
+
+        return this._router.isRouteActive(instruction);
     }
 }
 
