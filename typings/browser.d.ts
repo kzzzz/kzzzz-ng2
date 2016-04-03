62f698416a466f9922599651820d5f254c8a9e29
KZ <zhiping72@gmail.com>
2016-04-03 19:03:34 +0200

    TSD removed

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/lodash/index.d.ts" />
 /// <reference path="browser/ambient/underscore/index.d.ts" />
 /// <reference path="browser/definitions/lodash/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/lodash/index.d.ts" />
 /// <reference path="main/ambient/underscore/index.d.ts" />
 /// <reference path="main/definitions/lodash/index.d.ts" />
