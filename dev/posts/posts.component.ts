d432715f73916e2a6acf766f3f8e124e53abc750
KZ <zhiping72@gmail.com>
2016-04-03 02:13:10 +0200

    PostsComponent added

--- dev/posts/posts.component.ts
+++ dev/posts/posts.component.ts
@@ -1,10 +1,28 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {PostService} from './post.service';
 
 @Component({
     selector: 'posts',
-    template: `Posts`
+    templateUrl: './dev/posts/posts.template.html',
+    providers: [PostService]
 })
 
-export class PostsComponent{
+export class PostsComponent implements OnInit {
+
+    constructor(private _service:PostService) {
+
+    }
+
+    posts = [];
+
+    ngOnInit():any {
+      this.loadPosts();
+    }
+
+    private loadPosts(){
+        return this._service
+            .getPosts()
+            .subscribe(x => this.posts = x);
+    }
 
 }
